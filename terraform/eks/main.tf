2f6431d69e4f96bcee264a4cda8210cfe894ef7f
sivan marom <sivmamrom@gmail.com>
2023-06-26 14:35:03 +0300

    commit

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -92,3 +92,23 @@ resource "aws_eks_cluster" "eks_cluster" {
     aws_iam_role_policy_attachment.eks_cluster_policy_attachment
   ]
 }
+
+# Create the EKS node group
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "flask-nodes"
+  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+  subnet_ids      = aws_subnet.eks_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    min_size     = 1
+    max_size     = 5
+  }
+
+  instance_types = ["t2.micro"]
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
+}
