61cca3515fd5b5bee78a9fbf7e8f8580c77be1bd
sivan marom <sivmamrom@gmail.com>
2023-06-26 14:53:07 +0300

    changed eks

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -52,17 +52,22 @@ resource "aws_security_group" "eks_cluster_sg" {
 
 # Create the IAM role for the EKS cluster
 resource "aws_iam_role" "eks_cluster_role" {
-  name = var.eks_cluster_role
-
+  name               = "eks-cluster-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "",
       "Effect": "Allow",
       "Principal": {
         "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -74,7 +79,7 @@ EOF
 # Create the IAM role policy attachment for the EKS cluster
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = aws_iam_role.eks_cluster_role.assume_role_policy
 }
 
 # Create the EKS cluster
